d529faf65de43619a5052f1760137abf4452fa3d
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-11-15 10:08:07 +0100

    commit final 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-class Main {
+public class Main {
   public static void main(String[] args) {
     Voiture vt1 = new Voiture(Marque.Renault, "123AB81", 190, Modele.BERLINE, "Verte", 5);
     Voiture vt2 = new Voiture(Marque.Citroen, "1642BZ18", 140, Modele.BREAK);
